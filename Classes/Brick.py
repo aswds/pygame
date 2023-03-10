a1b4e2d0b4b9885ce1a70e242e42e4d16a9fb826
pinlok <s.morze@ukr.net>
2023-03-10 22:11:02 +0200

    Клас "Brick" містить конструктор __init__, який приймає на вхід 5 аргументів: x, y, width, height та color. Аргументи x та y відповідають координатам лівого верхнього кута цеглини в просторі гри. Аргумент width визначає ширину цеглини, а height - її висоту. Аргумент color визначає колір цеглини. У конструкторі ці аргументи зберігаються в відповідних полях об'єкту. Опис методів та функціоналу, що реалізований за допомогою цього класу, не надано.

---
+++ Classes/Brick.py
@@ -0,0 +1,8 @@
+class Brick:
+    def init(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
