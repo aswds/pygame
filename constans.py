82186212ae1b4cafb7f438f01118930b25886f85
aswds <sasha.iilllnitskiy@gmail.com>
2023-03-10 15:28:02 +0200

    Created constans

---
+++ constans.py
@@ -0,0 +1,19 @@
+import pygame
+
+WIDTH, HEIGHT = 600, 800
+BALL_RADIUS = 10
+PADDLE_WIDTH, PADDLE_HEIGHT = 100, 20
+BRICK_WIDTH, BRICK_HEIGHT = 50, 20
+BRICK_ROWS, BRICK_COLS = 5, 10
+BRICK_MARGIN = 10
+FONT_SIZE = 40
+
+# Set up the colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
