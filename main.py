626916cf3bf2873c031495120da3246f70155688
aswds <sasha.iilllnitskiy@gmail.com>
2023-04-27 19:43:51 +0300

    checking if os has a display

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pygame
 
 from main_menu import main_menu
+import os
+
+try:
+    os.environ["DISPLAY"]
+except:
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 if __name__ == "__main__":
     main_menu()
